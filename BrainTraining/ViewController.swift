3d4bf4ac9b3ab6d34493900c2406a06595e71023
tatiana-kn <tv.kniazeva@gmail.com>
2023-12-30 18:23:00 +0400

    Initial Commit

---
+++ BrainTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrainTraining
+//
+//  Created by Tia M on 12/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
