6d5374b9ae634e20dd48d2a327ca360f582c42be
tatiana-kn <123388284+tatiana-kn@users.noreply.github.com>
2024-01-19 17:51:22 +0400

    create TestScreen and Results storyboards and VCs

---
+++ BrainTraining/ViewControllers/MainScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenViewController.swift
+//  BrainTraining
+//
+//  Created by Tia M on 1/19/24.
+//
+
+import UIKit
+
+final class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
